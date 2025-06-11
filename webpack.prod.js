31abf3cb02f4ce8a5b45b9f0c51483b75d52bdac
Reiki Syah <reiky.mirzha321@gmail.com>
2025-06-11 18:03:45 +0800

    Optimize production webpack: extract CSS, split chunks, and minify for best build output

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,31 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.[contenthash].css',
+    }),
+  ],
+  optimization: {
+    splitChunks: { chunks: 'all' },
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({ parallel: true }),
+      new CssMinimizerPlugin(),
+    ],
+  },
 });
